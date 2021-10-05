957d0b9a192b9490ebe229c60c6e2e9ec77723f0
Brian Stiner <Brian.Stiner@revature.net>
2021-10-05 18:16:43 -0400

    Song

--- Program.cs
+++ Program.cs
@@ -11,7 +11,8 @@ namespace BrianStiner_C_Hello
             Console.WriteLine("Give me your name.");
             name = Console.ReadLine();
 
-            Console.WriteLine(&"Welcome to the jungle, {name}! ");
+            Console.WriteLine("Welcome to the jungle, we've got fun and games");
+            Console.WriteLine($"We got everything you want honey, we know the {name}");
 
         }
     }
